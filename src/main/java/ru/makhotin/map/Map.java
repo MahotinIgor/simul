aeb2a7c5a39f6960cc11ff341cef5753b3eaf183
Igor <mahotinigor@gmail.com>
2023-05-20 15:13:01 +0700

    Del: From Map delete method setupDefaultEntityPositions

--- src/main/java/ru/makhotin/map/Map.java
+++ src/main/java/ru/makhotin/map/Map.java
@@ -21,27 +21,7 @@ public class Map {
         entity.cell = cell;
         entitys.put(cell, entity);
     }
-    public void setupDefaultEntityPositions() {
-        for (int i = 0; i < 3; i++) {
-            setEntity(new Cell(new Random().nextInt(xMax), new Random().nextInt(yMax)),
-                    new Rock());
-        }
 
-        for (int i = 0; i < 3; i++) {
-            setEntity(new Cell(new Random().nextInt(xMax), new Random().nextInt(yMax)),
-                    new Grass());
-        }
-        for (int i = 0; i < 3; i++) {
-            setEntity(new Cell(new Random().nextInt(xMax), new Random().nextInt(yMax)),
-                    new Tree());
-        }
-
-        setEntity(new Cell(new Random().nextInt(xMax), new Random().nextInt(yMax)),
-                new Herbivore(10, 5 ));
-
-        setEntity(new Cell(new Random().nextInt(xMax), new Random().nextInt(yMax)),
-                new Carnivore( 10, 5 ,5));
-    }
     public boolean isEmptyCell(Cell cell) {
         return entitys.containsKey(cell);
     }
